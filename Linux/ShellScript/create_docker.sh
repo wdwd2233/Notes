8559d30c231a1fb715caf3e0e0b748e09ff1a7da
wdwd2233 <wdwd2233@gmail.com>
2019-08-28 11:03:13 +0800

    no message

---
+++ Linux/ShellScript/create_docker.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+source script_lib
+
+redis_container_name=my-redis
+
+getInnerIP static_ip
+
+function create_redis() {
+	echo 'create redis container...'
+
+	redis_host_data=/storage/redis/data
+	redis_container_data=/data
+	redis_port=6379
+
+	# 創建目錄 -p 多層創建 -m 權限
+	mkdir -p -m 711 $redis_host_data
+
+	docker run -e "TZ=Asia/Taipei" \
+		-v $redis_host_data:$redis_container_data \
+		--detach \
+		--publish $redis_port:$redis_port \
+		--name $redis_container_name redis
+	docker exec -it $redis_container_name bash -c "redis-server --version"
+
+	firewall-cmd --permanent --zone=public --add-port=$redis_port/tcp
+	firewall-cmd --reload
+}
+
+mysql_passwd=1234
+mysql_container_name=my-mysql
+mysql_docker_file_path=/root/DockerArea/MySQL/
+mysql_db_name=platform
+function create_mysql() {
+	# https://medium.com/@lvthillo/customize-your-mysql-database-in-docker-723ffd59d8fb
+	echo 'create MySQL container...'
+
+	mysql_host_data=/storage/mysql/data
+	mysql_container_data=/var/lib/mysql
+	mysql_port=3306
+
+	# 創建目錄 -p 多層創建 -m 權限
+	mkdir -p -m 711 /root/DockerArea/MySQL/sql-scripts
+	mkdir -p -m 711 $mysql_host_data
+
+	echo "FROM mysql" >>/root/DockerArea/MySQL/Dockerfile
+	echo "COPY ./sql-scripts/ /docker-entrypoint-initdb.d/" >>/root/DockerArea/MySQL/Dockerfile
+
+	# 移動至目錄
+	pushd $PWD
+	cd $mysql_docker_file_path
+
+	docker build -t my-mysql .
+	docker run -e "TZ=Asia/Taipei" \
+		-v $mysql_host_data:$mysql_container_data \
+		--detach \
+		--publish $mysql_port:$mysql_port \
+		-e MYSQL_ROOT_PASSWORD=$mysql_passwd \
+		--name $mysql_container_name my-mysql
+	docker exec -it $mysql_container_name bash -c "mysql -V"
+
+	# 返回目錄
+	popd
+
+	mysql_host_backup=/storage/mysql/backups
+	# 創建目錄 -p 多層創建 -m 權限
+	mkdir -p -m 711 $mysql_host_backup
+	(
+		crontab -l
+		echo "0 2 * * * /bin/docker exec "$mysql_container_name" sh -c 'exec mysqldump -uroot -p\"\$MYSQL_ROOT_PASSWORD\" "$mysql_db_name"' > "$mysql_host_backup"/"$mysql_db_name"-\`date +\"\%Y-\%m-\%d\"\`.sql"
+	) | crontab
+	(
+		crontab -l
+		echo "0 3 * * * find $mysql_host_backup/*.sql -mtime +6 -type f -delete"
+	) | crontab
+
+	firewall-cmd --permanent --zone=public --add-port=$mysql_port/tcp
+	firewall-cmd --reload
+}
+
+function create_nginx() {
+	echo 'create nginx...'
+
+	nginx_host_cfg=/root/DockerArea/nginx/nginx.conf
+	nginx_container_cfg=/etc/nginx/nginx.conf
+
+	nginx_host_client=/root/Server/env/client
+	nginx_container_client=/usr/share/nginx/html/client
+
+	nginx_port=8080
+
+	# 創建目錄 -p 多層創建 -m 權限
+	mkdir -p -m 711 /root/DockerArea/nginx
+	mkdir -p -m 711 $nginx_host_client
+
+	docker run --detach --name my-nginx nginx
+	docker cp my-nginx:$nginx_container_cfg $nginx_host_cfg
+	docker stop my-nginx
+	docker rm my-nginx
+	docker rmi nginx
+
+	docker run --detach \
+		-e "TZ=Asia/Taipei" \
+		-v $nginx_host_cfg:$nginx_container_cfg:ro \
+		-v $nginx_host_client:$nginx_container_client \
+		--publish $nginx_port:80 \
+		--name my-nginx nginx
+
+	firewall-cmd --permanent --zone=public --add-port=$nginx_port/tcp
+	firewall-cmd --reload
+}
+
+function redis_client() {
+	docker run -it --link $redis_container_name:$redis_container_name --rm redis sh -c 'exec redis-cli -h '$redis_container_name' -p 6379'
+}
+
+function menu() {
+	time=$(date '+%Y-%m-%d %H:%M:%S')
+	printc C_GREEN "================================================================\n"
+	printc C_GREEN "= create containers"
+	printc C_WHITE " (IP: $static_ip, time: $time)\n"
+	printc C_GREEN "================================================================\n"
+	printc C_CYAN "  1.  create_redis\n"
+	printc C_CYAN "  11. redis client\n"
+	printc C_CYAN "  2.  create_mysql\n"
+	printc C_CYAN "  3.  create_nginx\n"
+	printc C_CYAN "  q.  Exit\n"
+	while true; do
+		read -p "Please Select:" cmd
+		case $cmd in
+		1)
+			create_redis
+			return 0
+			;;
+		11)
+			redis_client
+			return 0
+			;;
+		2)
+			create_mysql
+			return 0
+			;;
+		3)
+			create_nginx
+			return 0
+			;;
+		[Qq]*)
+			return 1
+			;;
+		*)
+			echo "Please enter number or q to exit."
+			;;
+		esac
+	done
+}
+
+while menu; do
+	echo
+done
